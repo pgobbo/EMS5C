e56453c6faec4505f8e33d347d3109e1fe7f27a7
kubelsbeck.aline <kubelsbeck.aline@ITISLEVI.EDU>
2017-03-13 13:27:02 +0100

    Aggiunta locali

---
+++ controllers/controllerLocali.php
@@ -0,0 +1,109 @@
+<?php	
+require_once("models/modelLocali.php");
+
+	class controllerLocali{
+
+		function showHome()){
+			require_once("views/viewLocaliHome.php");
+		}
+
+		function showInput(){
+			require_once("views/viewLocaliInput.php");
+		}
+
+		function showFind(){
+			require_once("views/viewLocaliFind.php");
+		}
+
+		function showModify(){
+			require_once("./views/viewLocaliModify.php");
+		}
+
+		function add(){
+			$codice=$_POST['Codice'];
+			$descrizione=$_POST['Descrizione'];
+			$IdEdificio=$_POST['IdEdificio'];
+
+			$locale= new Locale(null,$codice,$descrizione,$IdEdificio);
+			if($locale->addLocale()){
+				echo"<script type='text/javascript'>alert('Inserimento andato a buon fine');</script>";
+			}else
+				echo"<script type='text/javascript'>alert('Inserimento non è andato a buon fine');</script>";
+
+			require_once("views/viewLocaliHome.php");
+		}
+
+		function find(){
+
+			$IdLocale="";
+			$Codice="";
+			$Descrizione="";
+			$IdEdificio="";
+
+		    if(isset($_GET['IdLocale'])){
+		        $IdLocale=$_GET['IdLocale'];
+		    }
+		    
+		    if(isset($_GET['Codice'])){
+		        $Codice=$_GET['Codice'];
+		    }
+
+		    if(isset($_GET['Descrizione'])){
+		        $Descrizione=$_GET['Descrizione'];
+		    }
+
+		    if(isset($_GET['IdEdificio'])){
+		        $IdEdificio=$_GET['IdEdificio'];
+		    }
+
+		    $locali=new Locali();
+		    $query=$locali->getFindQuery($IdLocale,$Codice,$Descrizione,$IdEdificio);
+		    
+		    $list_locali=$locali->getLocali($query);
+
+		    $_GET['locali']=$list_locali;
+		    require('./views/viewLocaliFind.php');
+		}
+
+		function createTable(){
+			
+			if(isset($_GET['locali'])){
+				foreach ($_GET['locali'] as $row=>$value) {
+					echo "<tr> <form method=POST action='index.php'>";
+						echo "<td align='center'> <input type='hidden' name='IdLocale' value='".$value->getId()."'>".$value->getId()."</td>";
+						echo "<td align='center'> <input type='hidden' name='Codice' value='".$value->getCodice()."'>".$value->getCodice()."</td>";
+						echo "<td align='center'> <input type='hidden' name='Descrizione' value='".$value->getDescrizione()."'>".$value->getDescrizione()."</td>";
+						echo "<td align='center'> <input type='hidden' name='IdEdificio' value='".$value->getIdEdificio()."'>".$value->getIdEdificio()."</td>";
+						echo "<td> <button> Scelgo Te! </button></td>";
+						echo "<input type='hidden' name='controller' value='controllerLocali'>";
+						echo "<input type='hidden' name='action' value='showModify'>";
+					echo "</form></tr>";
+				}
+			}
+		}
+	
+		
+
+		function delete(){
+			//print_r($_POST['IdLocale']);
+			if(Locali::delete($_POST['IdLocale'])){
+				echo("<script type='text/javascript'>alert('Ho rimosso il Prodotto ".$_POST['IdLocale']."!');</script>");
+				require_once("./views/home.php");
+			}else{
+				echo("<script type='text/javascript'>alert('NON SONO RIUSCITO A RIMUOVERE IL PRODOTTO ".$_POST['IdLocale']."! ');</script>");
+				require_once("./views/home.php");
+			}
+		}
+
+		function modify(){
+			if(Locali::modify()){
+				echo "<script type='text/javascript'>alert('fattissimooo!');</script>";
+				require_once("./views/viewLocaliModify.php");
+			}else{
+				
+			echo "<script type='text/javascript'>alert('NON SONO RIUSCITO AD AGGIORNARE IL PRODOTTO!".$_POST['IdLocale']."!');</script>";
+			require_once("./views/viewLocaliModify.php");
+			}
+		}
+    }
+?>
---
+++ views/viewLocaliInput.php
@@ -0,0 +1,21 @@
+<?php
+include("controllers/controllerLocali.php")
+?>
+<form action="index.php?controller=controllerLocali&action=add" method="POST">
+	<h1>Inserisci un nuovo Locale</h1>
+	Codice:<br>
+	<input type="text" name="Codice" placeholder="varchar(10)" required="required">
+	<br>
+	Descrizione:<br>
+	<input type="text" name="Descrizione" placeholder="varchar(100)" required="required" >
+	<br>
+	ID Edificio:<br>
+	<input type="text" name="IdEdificio" placeholder="int(11)" required="required">
+	<input type="submit" name="Inserisci">
+</form>
+<br>
+<form action="index.php" method="get">
+    <input type="hidden" name="controller" value="controllerLocali"></input>
+    <input type="hidden" name="action" value="showHome"></input>
+    <input type="submit" value="HOME" class="btn btn-primary">
+</form>
